58686b4b1f70db1b370ac680f9e3f94f76dd5750
Scott Rabin <scott@scottrabin.com>
2020-12-03 20:01:44 -0800

    Day 2, part 2
    Separated range password check from position password check as two
    different policy objects.

--- src/bin/day-2.rs
+++ src/bin/day-2.rs
@@ -9,7 +9,12 @@ fn main() -> Result<(), &'static str> {
             .captures(&line)
             .expect("valid password policy and password");
         (
-            PasswordPolicy::new(
+            RangePasswordPolicy::new(
+                captures[1].parse().unwrap(),
+                captures[2].parse().unwrap(),
+                captures[3].chars().next().unwrap(),
+            ),
+            PositionPasswordPolicy::new(
                 captures[1].parse().unwrap(),
                 captures[2].parse().unwrap(),
                 captures[3].chars().next().unwrap(),
@@ -18,22 +23,34 @@ fn main() -> Result<(), &'static str> {
         )
     });
 
-    let valid_passwords = input
-        .filter(|(policy, password)| policy.check_password(password))
-        .count();
-    println!("Valid passwords: {}", valid_passwords);
+    let valid_passwords = input.fold(
+        (0, 0),
+        |mut acc, (range_policy, position_policy, password)| {
+            if range_policy.check_password(&password) {
+                acc.0 += 1
+            }
+            if position_policy.check_password(&password) {
+                acc.1 += 1
+            }
+            acc
+        },
+    );
+
+    println!("Valid range passwords: {}", valid_passwords.0);
+    println!("Valid position passwords: {}", valid_passwords.1);
+
     Ok(())
 }
 
 #[derive(Debug)]
-struct PasswordPolicy {
+struct RangePasswordPolicy {
     count: std::ops::Range<usize>,
     letter: char,
 }
 
-impl PasswordPolicy {
+impl RangePasswordPolicy {
     fn new(min_count: usize, max_count: usize, letter: char) -> Self {
-        PasswordPolicy {
+        RangePasswordPolicy {
             count: std::ops::Range {
                 start: min_count,
                 end: max_count + 1,
@@ -48,16 +65,41 @@ impl PasswordPolicy {
     }
 }
 
+#[derive(Debug)]
+struct PositionPasswordPolicy {
+    indices: [usize; 2],
+    letter: char,
+}
+
+impl PositionPasswordPolicy {
+    fn new(first_position: usize, last_position: usize, letter: char) -> Self {
+        PositionPasswordPolicy {
+            indices: [first_position, last_position],
+            letter,
+        }
+    }
+
+    fn check_password(&self, password: &str) -> bool {
+        let first_position = password.chars().nth(self.indices[0] - 1);
+        let last_position = password.chars().nth(self.indices[1] - 1);
+
+        match (
+            first_position == Some(self.letter),
+            last_position == Some(self.letter),
+        ) {
+            (true, false) | (false, true) => true,
+            _ => false,
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn test_password_policy() {
-        let policy = PasswordPolicy {
-            count: std::ops::Range { start: 1, end: 6 },
-            letter: 'a',
-        };
+    fn test_range_password_policy() {
+        let policy = RangePasswordPolicy::new(1, 5, 'a');
         let cases = [
             ("", false),
             ("a", true),
@@ -80,6 +122,25 @@ mod test {
                 *result,
                 "Checking password '{}' against policy {:?}",
                 case,
+                policy
+            );
+        }
+    }
+
+    #[test]
+    fn test_position_password_policy() {
+        let cases = [
+            (PositionPasswordPolicy::new(1, 3, 'a'), "abcde", true),
+            (PositionPasswordPolicy::new(1, 3, 'b'), "cdefg", false),
+            (PositionPasswordPolicy::new(2, 9, 'c'), "ccccccccc", false),
+        ];
+
+        for (policy, password, result) in cases.iter() {
+            assert_eq!(
+                policy.check_password(password),
+                *result,
+                "Checking password '{}' against policy {:?}",
+                password,
                 policy
             );
         }
